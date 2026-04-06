4051ec21d0c45bd5770fc66941b7e38caa022f2d
tifffos2038-ctrl <tifffos2038@gmail.com>
2026-04-06 11:09:01 -0500

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -27,4 +27,6 @@ path('dealers/', TemplateView.as_view(template_name="index.html")),
 
 path('dealer/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
 
+path('postreview/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
