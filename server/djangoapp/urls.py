089c4ca21d4dd819b008a6729a12e07408f110da
tifffos2038-ctrl <tifffos2038@gmail.com>
2026-04-06 10:56:17 -0500

    Add dealer details and review paths to URLs

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -16,7 +16,14 @@ path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name
 path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
     path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
 
+path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
+
     # path for add a review view
 path(route='get_cars', view=views.get_cars, name ='getcars'),
 
+path(route='add_review', view=views.add_review, name='add_review'),
+
+path('dealers/', TemplateView.as_view(template_name="index.html")),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
