833ca33a553aee330d2058e1525dd27b100bf214
tifffos2038-ctrl <tifffos2038@gmail.com>
2026-04-06 10:57:04 -0500

    Add Dealers route to App component

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,14 @@
 import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
+import Dealers from './components/Dealers/Dealers';
+
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
           path('login/', TemplateView.as_view(template_name="index.html")),
+      <Route path="/dealers" element={<Dealers/>} />
 
     </Routes>
   );
