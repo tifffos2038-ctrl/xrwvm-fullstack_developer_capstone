7794d8813ea73f051cee635f95631775a14b0618
tifffos2038-ctrl <tifffos2038@gmail.com>
2026-04-06 11:04:32 -0500

    Update App.js

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,6 +1,7 @@
 import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
 import Dealers from './components/Dealers/Dealers';
+import Dealer from "./components/Dealers/Dealer"
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Route path="/login" element={<LoginPanel />} />
           path('login/', TemplateView.as_view(template_name="index.html")),
       <Route path="/dealers" element={<Dealers/>} />
+	  <Route path="/dealer/:id" element={<Dealer/>} />
 
     </Routes>
   );
