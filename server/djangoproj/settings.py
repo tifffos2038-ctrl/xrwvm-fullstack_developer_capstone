601dc41088ea818b6aa15109f2b96a84d1f0f75a
tifffos2038-ctrl <tifffos2038@gmail.com>
2026-04-06 08:49:38 -0500

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-CSRF_TRUSTED_ORIGINS = []
+ALLOWED_HOSTS = [['localhost','https://tifffos2038-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/']]
+CSRF_TRUSTED_ORIGINS = ['https://tifffos2038-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -134,5 +134,5 @@ MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = []
+STATICFILES_DIRS = [ os.path.join(BASE_DIR,'frontend/static')]
 
