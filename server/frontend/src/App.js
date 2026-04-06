ab76c11f1798d534bc64b96daa6035b0baa149de
tifffos2038-ctrl <tifffos2038@gmail.com>
2026-04-06 15:02:29 -0500

    Update App.js

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,21 +1,19 @@
-import LoginPanel from "./components/Login/Login"
-import { Routes, Route } from "react-router-dom";
-import Dealers from './components/Dealers/Dealers';
-import Dealer from "./components/Dealers/Dealer"
 import PostReview from "./components/Dealers/PostReview"
-
+import Dealer from "./components/Dealers/Dealer"
+import Dealers from './components/Dealers/Dealers';
+import LoginPanel from "./components/Login/Login"
+import Register from "./components/Register/Register"
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
-          path('login/', TemplateView.as_view(template_name="index.html")),
-      <Route path="/dealers" element={<Dealers/>} />
-	  <Route path="/dealer/:id" element={<Dealer/>} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dealer/:id" element={<Dealer/>} />
       <Route path="/postreview/:id" element={<PostReview/>} />
-
+      <Route path="/dealers" element={<Dealers/>} />
     </Routes>
   );
 }
 export default App;
-
